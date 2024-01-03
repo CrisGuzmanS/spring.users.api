ee4ea8d6fc92c4b201eedc07f517667ae9f0cca2
cris <cristian.guzman.contacto@gmail.com>
2024-01-03 15:49:52 +0000

    Dependency injection in UserController

--- src/main/java/com/tasks/tasks/controllers/UserController.java
+++ src/main/java/com/tasks/tasks/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.tasks.tasks.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ import com.tasks.tasks.models.User;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private com.tasks.tasks.dao.User user;
 
     @RequestMapping(value = "users")
     public List<User> users() {
