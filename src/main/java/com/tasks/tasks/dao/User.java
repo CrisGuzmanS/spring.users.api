3271ba954df0e25a5fc0185573b1c206aa3c0b2a
cris <cristian.guzman.contacto@gmail.com>
2024-01-03 05:45:12 +0000

    Entity manager in DAO

--- src/main/java/com/tasks/tasks/dao/User.java
+++ src/main/java/com/tasks/tasks/dao/User.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-// import jakarta.persistence.EntityManager;
-// import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Repository
 @Transactional
 public class User implements com.tasks.tasks.dao.interfaces.User {
 
-    // @PersistenceContext
-    // private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<com.tasks.tasks.dao.interfaces.User> users() {
