d20e870a8b1c133d3d1e3b369f0dded8f58e1f0c
cris <cristian.guzman.contacto@gmail.com>
2024-01-03 18:04:55 +0000

    User model must be an Entity

--- src/main/java/com/tasks/tasks/models/User.java
+++ src/main/java/com/tasks/tasks/models/User.java
@@ -1,11 +1,13 @@
 package com.tasks.tasks.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Table(name = "users")
 public class User {
     @Getter
