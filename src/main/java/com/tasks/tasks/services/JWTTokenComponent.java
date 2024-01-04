6fad2c93853894d308d9c6962e882712ee6c8e6c
cris <cristian.guzman.contacto@gmail.com>
2024-01-04 23:31:06 +0000

    removing unused SignatureAlgorithm

--- src/main/java/com/tasks/tasks/services/JWTTokenComponent.java
+++ src/main/java/com/tasks/tasks/services/JWTTokenComponent.java
@@ -3,7 +3,6 @@ package com.tasks.tasks.services;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
