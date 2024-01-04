dcae2a5eafa927828e4c265a0cd274dfbf936734
cris <cristian.guzman.contacto@gmail.com>
2024-01-04 17:28:17 +0000

    login() method recieve the User model

--- src/main/java/com/tasks/tasks/controllers/AuthController.java
+++ src/main/java/com/tasks/tasks/controllers/AuthController.java
@@ -1,15 +1,18 @@
 package com.tasks.tasks.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tasks.tasks.models.User;
 
 @RestController
 public class AuthController {
 
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public void login() {
-
+    public void login(@RequestBody User user) {
+        
     }
 
 }
