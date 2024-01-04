f965fe64eadc6b21485961158ea7150b4a4a6175
cris <cristian.guzman.contacto@gmail.com>
2024-01-04 17:09:58 +0000

    AuthController class has login() method

--- src/main/java/com/tasks/tasks/controllers/AuthController.java
+++ src/main/java/com/tasks/tasks/controllers/AuthController.java
@@ -1,8 +1,15 @@
 package com.tasks.tasks.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public void login() {
+
+    }
+
 }
