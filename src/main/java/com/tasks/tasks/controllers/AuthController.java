2e685c44bf6deb86a616a05eafb11b53ccc9bdc7
cris <cristian.guzman.contacto@gmail.com>
2024-01-04 17:29:56 +0000

    Autowired for DAO User

--- src/main/java/com/tasks/tasks/controllers/AuthController.java
+++ src/main/java/com/tasks/tasks/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.tasks.tasks.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,9 @@ import com.tasks.tasks.models.User;
 
 @RestController
 public class AuthController {
+
+    @Autowired
+    private com.tasks.tasks.dao.User user;
 
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public void login(@RequestBody User user) {
