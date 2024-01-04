1be31305583fe2c7f0019d99651a6b1a5e89f9af
cris <cristian.guzman.contacto@gmail.com>
2024-01-04 23:28:59 +0000

    RequestHeader Authorization in api/users

--- src/main/java/com/tasks/tasks/controllers/UserController.java
+++ src/main/java/com/tasks/tasks/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class UserController {
     private EncryptorService encryptor;
 
     @RequestMapping(value = "api/users")
-    public List<User> users() {
+    public List<User> users(@RequestHeader("Authorization") String token) {
         return user.get();
     }
 
